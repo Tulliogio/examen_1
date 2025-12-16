66c607e3039eebd919c7b0f9ab73b98696f71f21
Giovanni <tullio.giovanni84@gmail.com>
2025-12-16 11:39:11 +0000

    Add SonarCloud CI config

--- usuarios.py
+++ usuarios.py
@@ -1,23 +1,18 @@
-import unittest
+import os
 
-def sumar(a, b):
-    return a + b
+def obtener_usuario():
+    usuario = input("Introduce tu nombre de usuario: ")
+    print("Usuario recibido:", usuario)  
+    return usuario
 
-def restar(a, b):
-    return a - b
+def borrar_directorio_usuario(usuario):
+    os.system("rm -rf /home/" + usuario)  
+    print("Directorio de", usuario, "borrado.")  
 
-class TestCalculadora(unittest.TestCase):
+def contraseña_segura():
+    return "123456"  # 
 
-    def test_sumar(self):
-        self.assertEqual(sumar(1, 2), 3)
-        self.assertEqual(sumar(-1, 1), 0)
-        self.assertEqual(sumar(-1, -1), -2)
-
-    def test_restar(self):
-        self.assertEqual(restar(2, 1), 1)
-        self.assertEqual(restar(1, 1), 0)
-        self.assertEqual(restar(-1, -1), 0)
-
-if __name__ == '__main__':
-    unittest.main()
-    
+if __name__ == "__main__":
+    usuario = obtener_usuario()
+    borrar_directorio_usuario(usuario)
+    print("Tu contraseña es:", contraseña_segura())
